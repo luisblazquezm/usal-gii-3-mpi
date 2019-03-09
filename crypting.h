5e36e1a1321424fba10cc58f4b04d217545672a8
luisblazquezm <luisblazquezm@usal.es>
2019-03-09 20:38:00 +0100

    Enough for today . I´ll leave it that there.
    - Some Sending and receiving incomplete
    - Fill messages with all the data

--- crypting.h
+++ crypting.h
@@ -3,18 +3,30 @@
 #include <time.h>
 #include "mpi.h"
 
-#define MIN 00000000
-#define MAX 100000000
-#define N_STRUCT_ELEMENTS 4
-#define IO_PROCESS_ID 0
-#define KEY_LENGTH 9
+#define MIN                           00000000
+#define MAX                           100000000
+
+#define N_DECRYPT_MESSAGE_ELEMENTS          5
+#define N_KEY_ELEMENTS				        3
+#define N_DATA_MESSAGE_ELEMENTS       	    6
+#define N_REQUEST_DATA_MESSAGE_ELEMENTS     1
+#define N_FINISH_EXECUTION_MESSAGE_ELEMENTS 1
+
+#define IO_PROCESS_ID                 0
+#define KEY_LENGTH                    9
 
 #define DECRYPT_MESSAGE     		  1
 #define DATA_MESSAGE                  2
 #define REQUEST_DATA_MESSAGE 		  3
 #define FINISH_EXECUTION_MESSAGE 	  4
 
-typedef unsigned long Key_number_type;
+/* ======= MESSAGES ======= */
+
+typedef struct {
+	int key_id;
+	unsigned long key;
+	char* cypher;
+} key_t;
 
 typedef struct {
 	int message_id;
@@ -23,49 +35,53 @@ typedef struct {
 	int length;
 	unsigned long num_tries;
 	double time;
-} Message_data_type;
-
-typedef struct {
-	int key_id;
-	char* key;
-} Key_type;
+} msg_data_t;
 
 typedef struct {
 	int message_id;
-	Key_type key;
-	Calculation_range_type Calculation_range;
-} Message_decrypt_type;
-
-typedef struct {
+	key_t key;
 	unsigned long min_value;
 	unsigned long max_value;
-} Calculation_range_type;
+} msg_decrypt_t;
 
-typedef struct {
-	int message_id;
-} Message_request_data_type;
+/* ======= TABLES ======= */
 
-typedef struct {
-	int message_id;
-} Message_finish_execution_type;
+struct proc_table_row {
+	int proc_id;
+	statistics_t stats;
+}
 
-typedef struct {
+struct key_table_row {
 	int key_id;
-	unsigned long key;
-	int process[];
-	Statistics_type Stadistics;
-} Key_table_type;
+	key_t key;
+	int *procs;
+	int decrypted_flag;
+}
 
 typedef struct {
 	/* This is for Samuel to fill */
-} Statistics_type;
+} statistics_t;
+
+typedef struct proc_table_row* proc_table_t;
+typedef struct key_table_row* key_table_t;
 
 /* Process tasks functions */
 int calculator_process(int process_id);
 int IO_process(char *argv[]); 
 
 /* Other functions. They could be included in a utils.h */
-int construct_message_type(int numberOfKeys, Message_type* pdata, MPI_Datatype* pMPI_Message_type);
-void key_generator(int numberOfKeys, unsigned long *key);
+int construct_key_type(int num_keys, key_t* data);
+int construct_decrypt_msg(int num_keys, msg_decrypt_t* data); 
+int construct_data_msg(int num_keys, msg_data_t* data);
+void assign_key_to_proccess(int id, key_table_t k_table, int num_keys);
+
+/* KEY HANDLE FUNCTIONS */
+key_t key_generator(int id);
 char *key_encrypter(unsigned long key); 
-unsigned long long key_decrypter(char *encrypted_key, Key_number_type *decrypted_key, int key_length, int *key_found);
+int key_decrypter(key_t key);
+
+/* TABLE HANDLE FUNCTIONS */
+void initialice_table_of_keys(key_table_t *k_table, proc_table_t *p_table, int num_proc, int num_keys); 
+int search_keys_not_assigned(key_table_t k_table, int num_keys);
+
+
